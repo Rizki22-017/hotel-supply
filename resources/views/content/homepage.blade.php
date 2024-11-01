8d6191ffb32d76a881edbb98862d6d4c130392f7
Rizki22-017 <126841212+Rizki22-017@users.noreply.github.com>
2024-11-01 10:13:30 +0700

    homepage

--- resources/views/content/homepage.blade.php
+++ resources/views/content/homepage.blade.php
@@ -1,6 +1,41 @@
 @extends('layout.app')
 @section('content')
 
-    
+    <!-- Hero Section -->
+    <section id="hero" class="hero section light-background">
+
+        <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+
+        <div class="container position-relative">
+
+
+
+          <div class="content row gy-4">
+
+
+            <div class="col-lg-8 d-flex align-items-stretch">
+              <div class="d-flex flex-column justify-content-center">
+                <div class="row gy-4">
+
+                    <div class="hero-content" data-aos="fade-up" data-aos-delay="200">
+
+                        <h1 class="mb-4">
+                          Everything you need <br>
+                          For hotel linen
+                        </h1>
+
+                        <div class="hero-buttons">
+                          <a href="#about" class="btn btn-primary me-0 me-sm-2 mx-1">Browse Now!</a>
+                        </div>
+                      </div>
+
+                </div>
+              </div>
+            </div>
+          </div><!-- End  Content-->
+
+        </div>
+
+      </section><!-- /Hero Section -->
 
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - Medilab Bootstrap Template</title>
+  <title>360 Hotel Supply</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
@@ -28,13 +28,6 @@
   <!-- Main CSS File -->
   <link href="assets/css/main.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: Medilab
-  * Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body class="index-page">
@@ -53,33 +46,16 @@
         <nav id="navmenu" class="navmenu">
           <ul>
             <li><a href="#hero" class="active">Home<br></a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#departments">Departments</a></li>
-            <li><a href="#doctors">Doctors</a></li>
-            <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-              <ul>
-                <li><a href="#">Dropdown 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Dropdown 1</a></li>
-                    <li><a href="#">Deep Dropdown 2</a></li>
-                    <li><a href="#">Deep Dropdown 3</a></li>
-                    <li><a href="#">Deep Dropdown 4</a></li>
-                    <li><a href="#">Deep Dropdown 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Dropdown 2</a></li>
-                <li><a href="#">Dropdown 3</a></li>
-                <li><a href="#">Dropdown 4</a></li>
-              </ul>
-            </li>
+            <li><a href="#about">Products</a></li>
+            <li><a href="#services">Gallery</a></li>
+            <li><a href="#departments">Our Client</a></li>
+            <li><a href="#doctors">About</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
           <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <a class="cta-btn d-none d-sm-block" href="#appointment">Make an Appointment</a>
+        <a class="cta-btn d-none d-sm-block" href="#appointment">Order Now</a>
 
       </div>
 
