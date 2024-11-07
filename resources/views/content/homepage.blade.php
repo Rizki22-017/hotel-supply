e6d24a0461339190ecd7fdfab6efb83a06f33c92
Rizki22-017 <126841212+Rizki22-017@users.noreply.github.com>
2024-11-07 11:17:11 +0700

    fe-updateclient

--- resources/views/content/homepage.blade.php
+++ resources/views/content/homepage.blade.php
@@ -502,72 +502,199 @@
 
     <!-- Clients Section -->
     <section id="clients" class="clients section light-background">
-        <!-- Section Title -->
-        <div class="container section-title" data-aos="fade-up">
-            <h2>Client</h2>
-            <p>We provide a wide range of customized linen and towel products with various specification and
-                requirements.<br></p>
-        </div><!-- End Section Title -->
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="container">
+            <!-- Section Title -->
+            <div class="container section-title" data-aos="fade-up">
+                <h2>Client</h2>
+                <p>We provide a wide range of customized linen and towel products with various specification and
+                    requirements.<br></p>
+            </div><!-- End Section Title -->
 
             <div class="swiper init-swiper">
                 <script type="application/json" class="swiper-config">
-              {
-                "loop": true,
-                "speed": 400,
-                "autoplay": {
-                  "delay": 2000
-                },
-                "slidesPerView": "auto",
-                "pagination": {
-                  "el": ".swiper-pagination",
-                  "type": "bullets",
-                  "clickable": true
-                },
-                "breakpoints": {
-                  "320": {
-                    "slidesPerView": 2,
-                    "spaceBetween": 40
-                  },
-                  "480": {
-                    "slidesPerView": 3,
-                    "spaceBetween": 60
-                  },
-                  "640": {
-                    "slidesPerView": 4,
-                    "spaceBetween": 80
-                  },
-                  "992": {
-                    "slidesPerView": 6,
-                    "spaceBetween": 120
-                  }
+                {
+                    "loop": true,
+                    "speed": 600,
+                    "autoplay": {
+                    "delay": 5000
+                    },
+                    "slidesPerView": "auto",
+                    "pagination": {
+                    "el": ".swiper-pagination",
+                    "type": "bullets",
+                    "clickable": true
+                    },
+                    "breakpoints": {
+                    "320": {
+                        "slidesPerView": 2,
+                        "spaceBetween": 40
+                    },
+                    "480": {
+                        "slidesPerView": 3,
+                        "spaceBetween": 60
+                    },
+                    "640": {
+                        "slidesPerView": 4,
+                        "spaceBetween": 80
+                    },
+                    "992": {
+                        "slidesPerView": 6,
+                        "spaceBetween": 120
+                    }
+                    }
                 }
-              }
-            </script>
+                </script>
                 <div class="swiper-wrapper align-items-center">
-                    <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid"
-                            alt=""></div>
-                    <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid"
-                            alt=""></div>
-                    <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid"
-                            alt=""></div>
-                    <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid"
-                            alt=""></div>
-                    <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid"
-                            alt=""></div>
-                    <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid"
-                            alt=""></div>
-                    <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid"
-                            alt=""></div>
-                    <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid"
-                            alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
                 </div>
-                <div class="swiper-pagination"></div>
             </div>
-
+            <div class="swiper init-swiper">
+                <script type="application/json" class="swiper-config">
+                {
+                    "loop": true,
+                    "speed": 700,
+                    "autoplay": {
+                    "delay": 6000
+                    },
+                    "slidesPerView": "auto",
+                    "pagination": {
+                    "el": ".swiper-pagination",
+                    "type": "bullets",
+                    "clickable": true
+                    },
+                    "breakpoints": {
+                    "320": {
+                        "slidesPerView": 2,
+                        "spaceBetween": 40
+                    },
+                    "480": {
+                        "slidesPerView": 3,
+                        "spaceBetween": 60
+                    },
+                    "640": {
+                        "slidesPerView": 4,
+                        "spaceBetween": 80
+                    },
+                    "992": {
+                        "slidesPerView": 6,
+                        "spaceBetween": 120
+                    }
+                    }
+                }
+                </script>
+                <div class="swiper-wrapper align-items-center">
+                    <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
+                </div>
+            </div>
+            <div class="swiper init-swiper">
+                <script type="application/json" class="swiper-config">
+                {
+                    "loop": true,
+                    "speed": 800,
+                    "autoplay": {
+                    "delay": 7000
+                    },
+                    "slidesPerView": "auto",
+                    "pagination": {
+                    "el": ".swiper-pagination",
+                    "type": "bullets",
+                    "clickable": true
+                    },
+                    "breakpoints": {
+                    "320": {
+                        "slidesPerView": 2,
+                        "spaceBetween": 40
+                    },
+                    "480": {
+                        "slidesPerView": 3,
+                        "spaceBetween": 60
+                    },
+                    "640": {
+                        "slidesPerView": 4,
+                        "spaceBetween": 80
+                    },
+                    "992": {
+                        "slidesPerView": 6,
+                        "spaceBetween": 120
+                    }
+                    }
+                }
+                </script>
+                <div class="swiper-wrapper align-items-center">
+                    <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
+                </div>
+            </div>
+            <div class="swiper init-swiper">
+                <script type="application/json" class="swiper-config">
+                {
+                    "loop": true,
+                    "speed": 900,
+                    "autoplay": {
+                    "delay": 8000
+                    },
+                    "slidesPerView": "auto",
+                    "pagination": {
+                    "el": ".swiper-pagination",
+                    "type": "bullets",
+                    "clickable": true
+                    },
+                    "breakpoints": {
+                    "320": {
+                        "slidesPerView": 2,
+                        "spaceBetween": 40
+                    },
+                    "480": {
+                        "slidesPerView": 3,
+                        "spaceBetween": 60
+                    },
+                    "640": {
+                        "slidesPerView": 4,
+                        "spaceBetween": 80
+                    },
+                    "992": {
+                        "slidesPerView": 6,
+                        "spaceBetween": 120
+                    }
+                    }
+                }
+                </script>
+                <div class="swiper-wrapper align-items-center">
+                    <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+                    <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
+                </div>
+            </div>
         </div>
-
     </section><!-- /Clients Section -->
+
+
 
     <!-- Testimonials Section -->
     <section id="testimonials" class="testimonials section light-background">
